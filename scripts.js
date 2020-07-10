ef7b81faacf0fe46e6c2119ca2471f937880e0a7
atakygan <atakygan>
2020-07-10 15:38:50 +0200

    bemelegito gyakorlas

---
+++ scripts.js
@@ -0,0 +1,12 @@
+console.log("JS is working");
+let age=33;
+
+if (age>18) {
+    console.log ("you are an adult") 
+} else{
+    console.log ("u kid")
+}
+for (let i = 0;  i< 10; i++) {
+    console.log(i)  
+    
+}
